e6f3ccfc00d33354cc3ac3b06de191d5f6357a30
Антон <121505643+SlowerTK@users.noreply.github.com>
2024-07-19 22:56:25 +0700

    Add files via upload

--- Application.cpp
+++ Application.cpp
@@ -1,5 +1,7 @@
 #include "Application.h"
 
+
+
 Application::Window::Window(std::string title, int x, int y, int w, int h) {
 	this->w = w;
 	this->h = h;
@@ -43,22 +45,81 @@ void Application::Window::init(std::string title, int x, int y, int w, int h) {
 	}
 }
 
-int Application::Window::eventChecker()
-{
+int Application::Window::eventChecker() {
 	while (SDL_PollEvent(&e) != 0) {
 		if (e.type == SDL_QUIT) {
 			return aexit;
 		}
 		if (e.type == SDL_MOUSEBUTTONDOWN) {
 			if (e.button.button == SDL_BUTTON_LEFT) {
-				if (e.button.x > w / 16 && e.button.x < w / 16 + w / 8 && e.button.y > h / 16 && e.button.y < h / 16 + h / 16) {
+				if (e.button.x > w / 24 && e.button.x < w / 24 + w / 6 && e.button.y > h / 16 && e.button.y < h / 16 + h / 16) {
 					SDL_Log("Pause");
 					return pause;
 				}
-				if (e.button.x > w / 16 && e.button.x < w / 16 + w / 8 && e.button.y > h / 16 * 3 && e.button.y < h / 16 * 3 + h / 16) {
-					SDL_Log("Start");
-					return start;
+				if (e.button.x > w / 24 && e.button.x < w / 24 + w / 6 && e.button.y > h / 16 * 2 + 10 && e.button.y < h / 16 * 2 + 10 + h / 16) {
+					SDL_Log("Restart");
+					return restart;
 				}
+				if (e.button.x > w / 24 && e.button.x < w / 24 + w / 6 && e.button.y > h / 16 * 3 + 20 && e.button.y < h / 16 * 3 + 20 + h / 16) {
+					SDL_Log("Info");
+					return info;
+				}
+			}
+		}
+		if (e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEMOTION) {
+			if (e.button.button == SDL_BUTTON_LEFT) {
+				int x = e.button.x;
+				int y = e.button.y;
+
+				if (y >= h / 2 - 50 - 15 && y <= h / 2 - 50 + 15) {
+					if (x >= 10 && x <= 10 + (w / 4 - 20)) {
+						return(SLIDER1_MIN + (x - 10) * (SLIDER1_MAX - SLIDER1_MIN) / (w / 4 - 20));
+					}
+				}
+				if (y >= h / 2 + 50 - 15 && y <= h / 2 + 50 + 15) {
+					if (x >= 10 && x <= 10 + (w / 4 - 20)) {
+						return(SLIDER1_MIN + (x - 10) * (SLIDER1_MAX - SLIDER1_MIN) / (w / 4 - 20) + 100);
+					}
+				}
+				if (y >= h / 2 + 150 - 15 && y <= h / 2 + 150 + 15) {
+					if (x >= 10 && x <= 10 + (w / 4 - 20)) {
+						return(SLIDER2_MIN + (x - 10) * (SLIDER2_MAX - SLIDER2_MIN) / (w / 4 - 20) + 201);
+					}
+				}
+				if (y >= h / 2 + 250 - 15 && y <= h / 2 + 250 + 15) {
+					if (x >= 10 && x <= 10 + (w / 4 - 20)) {
+						return(SLIDER2_MIN + (x - 10) * (SLIDER2_MAX - SLIDER2_MIN) / (w / 4 - 20) + 562);
+					}
+				}
+			}
+		}
+		if (e.type == SDL_MOUSEWHEEL) {
+			int x, y;
+			SDL_GetMouseState(&x, &y);
+
+			if (y >= h / 2 - 50 - 15 && y <= h / 2 - 50 + 15) {
+				if (e.wheel.y > 0)
+					return incMass1;
+				else if (e.wheel.y < 0)
+					return decMass1;
+			}
+			if (y >= h / 2 + 50 - 15 && y <= h / 2 + 50 + 15) {
+				if (e.wheel.y > 0)
+					return incMass2;
+				else if (e.wheel.y < 0)
+					return decMass2;
+			}
+			if (y >= h / 2 + 150 - 15 && y <= h / 2 + 150 + 15) {
+				if (e.wheel.y > 0)
+					return incAngle1;
+				else if (e.wheel.y < 0)
+					return decAngle1;
+			}
+			if (y >= h / 2 + 250 - 15 && y <= h / 2 + 250 + 15) {
+				if (e.wheel.y > 0)
+					return incAngle2;
+				else if (e.wheel.y < 0)
+					return decAngle2;
 			}
 		}
 		if (e.type == SDL_KEYDOWN) {
@@ -78,53 +139,73 @@ int Application::Window::eventChecker()
 void Application::Window::Renderer(std::vector<Ball>& b, int pressedButton) const {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
-	drawLeftMenu(b);
+	drawLeftMenu(b, pressedButton);
 	drawPlatform();
 	drawBall(b);
 	drawLineFromPlatformToBall(b);
 	SDL_RenderPresent(renderer);
 }
 
-void Application::Window::drawLeftMenu(std::vector<Ball>& b) const{
+void Application::Window::drawLeftMenu(std::vector<Ball>& b, int pressedButton) const {
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_Rect leftMenu = { 0, 0, w / 4, h };
 	SDL_RenderFillRect(renderer, &leftMenu);
 
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_Rect stopButton = { w / 16, h / 16, w / 8, h / 16 };
+	SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+	SDL_Rect stopButton = { w / 24, h / 16, w / 6, h / 16 };
 	SDL_RenderFillRect(renderer, &stopButton);
 	SDL_Color color = { 255, 255, 255, 255 };
 	SDL_Surface* surface = TTF_RenderText_Solid(font, "Pause", color);
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_Rect textRect = { stopButton.x + w / 72, stopButton.y + h / 32 - surface->h / 2, surface->w, surface->h };
+	SDL_Rect textRect = { stopButton.x + w / 32, stopButton.y + h / 32 - surface->h / 2, surface->w, surface->h };
 	SDL_RenderCopy(renderer, texture, NULL, &textRect);
 	SDL_FreeSurface(surface);
 	SDL_DestroyTexture(texture);
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_Rect startButton = { w / 16, h / 16 * 3, w / 8, h / 16 };
-	SDL_RenderFillRect(renderer, &startButton);
-	surface = TTF_RenderText_Solid(font, "Start", color);
+	SDL_Rect restartButton = { w / 24, h / 16 * 2 + 10, w / 6, h / 16 };
+	SDL_RenderFillRect(renderer, &restartButton);
+	surface = TTF_RenderText_Solid(font, "Restart", color);
 	texture = SDL_CreateTextureFromSurface(renderer, surface);
-	textRect = { startButton.x + w / 64, startButton.y + h / 32 - surface->h / 2, surface->w, surface->h };
+	textRect = { restartButton.x + w / 64, restartButton.y + h / 32 - surface->h / 2, surface->w, surface->h };
 	SDL_RenderCopy(renderer, texture, NULL, &textRect);
 	SDL_FreeSurface(surface);
 	SDL_DestroyTexture(texture);
 
+	SDL_Rect infoButton = { w / 24, h / 16 * 3 + 20, w / 6, h / 16 };
+	SDL_RenderFillRect(renderer, &infoButton);
+	surface = TTF_RenderText_Solid(font, "Info", color);
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	textRect = { infoButton.x + w / 24, infoButton.y + h / 32 - surface->h / 2, surface->w, surface->h };
+	SDL_RenderCopy(renderer, texture, NULL, &textRect);
+	SDL_FreeSurface(surface);
+	SDL_DestroyTexture(texture);
+	static bool isInfo = false;
+	if (pressedButton == info) {
+		isInfo = !isInfo;
+	}
+	if (isInfo) {
+		drawInfo(b);
+	}
+	else {
+		drawControls(b);
+	}
+}
+
+void Application::Window::drawInfo(std::vector<Ball>& b) const {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderDrawLine(renderer, 0, h / 16 * 5, w/4-1, h / 16 * 5);
+	SDL_RenderDrawLine(renderer, 0, h / 16 * 5, w / 4 - 1, h / 16 * 5);
 	TTF_SetFontSize(font, 14);
 	SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 	SDL_Color color2 = { 0, 0, 0, 255 };
-	int ballCount = 0;
+	int ballCount = 1;
 	int i = 0;
 	for (auto& ball : b) {
-		SDL_Rect ballInfo = { 0, h / 16 * 5 + 1+ i, w / 4, h / 16 };
+		SDL_Rect ballInfo = { 0, h / 16 * 5 + 1 + i, w / 4, h / 16 };
 		SDL_RenderFillRect(renderer, &ballInfo);
 		std::string ballInfoText = "Ball " + std::to_string(ballCount);
-		surface = TTF_RenderText_Solid(font, ballInfoText.c_str(), color2);
-		texture = SDL_CreateTextureFromSurface(renderer, surface);
-		textRect = { ballInfo.x + w / 64, ballInfo.y + h / 32 - surface->h / 2, surface->w, surface->h };
+		SDL_Surface* surface = TTF_RenderText_Solid(font, ballInfoText.c_str(), color2);
+		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_Rect textRect = { ballInfo.x + w / 64, ballInfo.y + h / 32 - surface->h / 2, surface->w, surface->h };
 		SDL_RenderCopy(renderer, texture, NULL, &textRect);
 		SDL_FreeSurface(surface);
 		SDL_DestroyTexture(texture);
@@ -134,29 +215,31 @@ void Application::Window::drawLeftMenu(std::vector<Ball>& b) const{
 		ballInfoText = "Mass: " + std::to_string(ball.mass);
 		surface = TTF_RenderText_Solid(font, ballInfoText.c_str(), color2);
 		texture = SDL_CreateTextureFromSurface(renderer, surface);
-		textRect = { ballInfo.x + w / 16, ballInfo.y + h / 32 - surface->h / 2, surface->w, surface->h };
+		textRect = { ballInfo.x + w / 24, ballInfo.y + h / 32 - surface->h / 2, surface->w, surface->h };
 		SDL_RenderCopy(renderer, texture, NULL, &textRect);
 		SDL_FreeSurface(surface);
 		SDL_DestroyTexture(texture);
 
 		ballInfo.y += h / 16;
 		SDL_RenderFillRect(renderer, &ballInfo);
-		double speed = ball.omega * 180/M_PI;
+		double speed = /*RadToDeg*/(ball.omega);
 		ballInfoText = "Speed: " + std::to_string(abs(speed));
 		surface = TTF_RenderText_Solid(font, ballInfoText.c_str(), color2);
 		texture = SDL_CreateTextureFromSurface(renderer, surface);
-		textRect.y += h / 16;
+		textRect = { ballInfo.x + w / 24, ballInfo.y + h / 32 - surface->h / 2, surface->w, surface->h };
 		SDL_RenderCopy(renderer, texture, NULL, &textRect);
 		SDL_FreeSurface(surface);
 		SDL_DestroyTexture(texture);
 
 		ballInfo.y += h / 16;
 		SDL_RenderFillRect(renderer, &ballInfo);
-		double angle = ball.theta * 180 / M_PI;
+		double angle = RadToDeg(ball.theta);
+		while (angle > 360) { angle -= 360; }
+		while (angle < -360) { angle += 360; }
 		ballInfoText = "Angle: " + std::to_string(angle);
 		surface = TTF_RenderText_Solid(font, ballInfoText.c_str(), color2);
 		texture = SDL_CreateTextureFromSurface(renderer, surface);
-		textRect.y += h / 16;
+		textRect = { ballInfo.x + w / 24, ballInfo.y + h / 32 - surface->h / 2, surface->w, surface->h };
 		SDL_RenderCopy(renderer, texture, NULL, &textRect);
 		SDL_FreeSurface(surface);
 		SDL_DestroyTexture(texture);
@@ -173,14 +256,71 @@ void Application::Window::drawLeftMenu(std::vector<Ball>& b) const{
 		SDL_DestroyTexture(texture);
 
 		++ballCount;
-		i += 5*h/16;
+		i += 5 * h / 16;
 	}
 	TTF_SetFontSize(font, 32);
 }
 
+void Application::Window::drawControls(std::vector<Ball>& b) const {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderDrawLine(renderer, 0, h / 16 * 5, w / 4 - 1, h / 16 * 5);
+	int i = 0;
+	int BallCount = 1;
+	for (auto& ball : b) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_Rect track = { 10, (h / 2 - 50) + i, (w / 4 - 20), SLIDER_HEIGHT };
+		SDL_RenderFillRect(renderer, &track);
+
+		SDL_Rect handle = { 10 + static_cast<int>(ball.mass * (w / 4 - 20) / (SLIDER1_MAX - SLIDER1_MIN)) - 5, (h / 2 - 50) - 13 + i, 10, 30 };
+		SDL_RenderFillRect(renderer, &handle);
+
+		SDL_Color textColor = { 0, 0, 0, 255 };
+		std::string text = "Mass " + std::to_string(BallCount) + ": " + std::to_string(static_cast<int>(ball.mass));
+		SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), textColor);
+		SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+		int textWidth = static_cast<int>(textSurface->w / 1.5);
+		int textHeight = static_cast<int>(textSurface->h / 1.5);
+
+		SDL_Rect renderQuad = { 10, h / 3 + i, textWidth, textHeight };
+		SDL_RenderCopy(renderer, textTexture, nullptr, &renderQuad);
+
+		SDL_FreeSurface(textSurface);
+		SDL_DestroyTexture(textTexture);
+		++BallCount;
+		i += 100;
+	}
+	BallCount = 1;
+	for (auto& ball : b) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_Rect track = { 10, (h / 2 - 50) + i, (w / 4 - 20), SLIDER_HEIGHT };
+		SDL_RenderFillRect(renderer, &track);
+
+		double angle = RadToDeg(ball.theta);
+		while (angle > 360) { angle -= 360; }
+		while (angle < 0) { angle += 360; }
+		SDL_Rect handle = { 10 + static_cast<int>(angle * (w / 4 - 20) / (SLIDER2_MAX - SLIDER2_MIN)) - 5, (h / 2 - 50) - 13 + i, 10, 30 };
+		SDL_RenderFillRect(renderer, &handle);
+
+		SDL_Color textColor = { 0, 0, 0, 255 };
+		std::string text = "Angle " + std::to_string(BallCount) + ": " + std::to_string(static_cast<int>(angle));
+		SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), textColor);
+		SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+		int textWidth = static_cast<int>(textSurface->w / 1.5);
+		int textHeight = static_cast<int>(textSurface->h / 1.5);
+
+		SDL_Rect renderQuad = { 10, h / 3 + i, textWidth, textHeight };
+		SDL_RenderCopy(renderer, textTexture, nullptr, &renderQuad);
+
+		SDL_FreeSurface(textSurface);
+		SDL_DestroyTexture(textTexture);
+		++BallCount;
+		i += 100;
+	}
+}
+
 void Application::Window::drawPlatform() const {
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_Rect platform = { w * 6 / 12, h / 6, w / 4, h / 12 };
+	SDL_Rect platform = { w * 6 / 12, h / 6, w / 4, h / 24 };
 	SDL_RenderFillRect(renderer, &platform);
 }
 
@@ -218,32 +358,50 @@ void Application::Window::drawBall(std::vector<Ball>& b) const {
 	}
 }
 
-void Application::Window::drawLineFromPlatformToBall(std::vector<Ball>& b) const{
+void Application::Window::drawLineFromPlatformToBall(std::vector<Ball>& b) const {
 	for (auto& ball : b) {
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		SDL_RenderDrawLine(renderer, ball.px, ball.py, static_cast<int>(ball.x), static_cast<int>(ball.y));
 	}
 }
 
-void Application::Window::moveBall(std::vector<Ball>& b, double dt, int pressedButton){
+void Application::Window::moveBall(std::vector<Ball>& b, double dt, int pressedButton) {
+	static double startPoint0 = b[0].theta;
+	static double startX0 = b[0].px + b[0].len * sin(startPoint0);
+	static double startY0 = b[0].py + b[0].len * cos(startPoint0);
+	static double startPoint1 = b[1].theta;
+	static double startX1 = b[1].px + b[1].len * sin(startPoint1);
+	static double startY1 = b[1].py + b[1].len * cos(startPoint1);
+	if (pressedButton == restart) {
+		b[0].theta = startPoint0;
+		b[0].alpha = 0;
+		b[0].omega = 0;
+		b[0].x = startX0;
+		b[0].y = startY0;
+		b[1].theta = startPoint1;
+		b[1].alpha = 0;
+		b[1].omega = 0;
+		b[1].x = startX1;
+		b[1].y = startY1;
+	}
 	static bool moving = true;
 	if (pressedButton == pause)
 		moving = !moving;
-	if (moving || pressedButton == next) {
+	if (moving || pressedButton == next)
 		for (auto& ball : b) {
 			ball.alpha = -(g / ball.len) * sin(ball.theta);
 			ball.omega += ball.alpha * dt;
 			ball.theta += ball.omega * dt;
-
-			ball.x = ball.px + ball.len * sin(ball.theta);
-			ball.y = ball.py + ball.len * cos(ball.theta);
-
 		}
-		resolveCollision(b);
+	for (auto& ball : b) {
+		ball.x = ball.px + ball.len * sin(ball.theta);
+		ball.y = ball.py + ball.len * cos(ball.theta);
 	}
+	resolveCollision(b);
+	changeRadiusFromMass(b);
 }
 
-void Application::Window::resolveCollision(std::vector<Ball>& b){
+void Application::Window::resolveCollision(std::vector<Ball>& b) {
 	Ball ball1 = b[0];
 	Ball ball2 = b[1];
 	double dist = std::sqrt((ball1.x - ball2.x) * (ball1.x - ball2.x) + (ball1.y - ball2.y) * (ball1.y - ball2.y));
@@ -252,11 +410,6 @@ void Application::Window::resolveCollision(std::vector<Ball>& b){
 		double angle = std::atan2(ball2.y - ball1.y, ball2.x - ball1.x);
 		double sine = std::sin(angle);
 		double cosine = std::cos(angle);
-
-		double x1 = 0;
-		double y1 = 0;
-		double x2 = dist * cosine;
-		double y2 = dist * sine;
 
 		double vx1 = ball1.omega * ball1.len * cosine;
 		double vy1 = ball1.omega * ball1.len * sine;
@@ -270,13 +423,21 @@ void Application::Window::resolveCollision(std::vector<Ball>& b){
 		ball2.omega = (vx2Final * cosine - vy2 * sine) / ball2.len;
 
 
-		double separation = overlap / 2;
+		double separation = 0.5 * (overlap + 0.01);
 		ball1.x -= separation * cosine;
 		ball1.y -= separation * sine;
 		ball2.x += separation * cosine;
 		ball2.y += separation * sine;
-		
+
 		b[0] = ball1;
 		b[1] = ball2;
 	}
+}
+
+
+
+void Application::Window::changeRadiusFromMass(std::vector<Ball>& b) {
+	for (auto& ball : b)
+		if (ball.mass <= 15) ball.r = 15;
+		else ball.r = 15 + (ball.mass - 15) * (65 - 15) / (100 - 15);
 }
--- Application.h
+++ Application.h
@@ -5,16 +5,33 @@
 #include <string>
 #include <vector>
 
+#define DegToRad(x) ((x) * M_PI / 180)
+#define RadToDeg(x) ((x) * 180 / M_PI)
+
 constexpr double g = 9.81;
+const int SLIDER_HEIGHT = 10;
+const int SLIDER1_MIN = 1;
+const int SLIDER1_MAX = 100;
+const int SLIDER2_MIN = 0;
+const int SLIDER2_MAX = 359;
 
 class Application
 {
 	enum buttons {
-		nothing,
+		nothing = 922,
 		pause,
 		next,
 		stop,
-		start,
+		restart,
+		info,
+		incMass1,
+		decMass1,
+		incMass2,
+		decMass2,
+		incAngle1,
+		decAngle1,
+		incAngle2,
+		decAngle2,
 		aexit
 	};
 
@@ -31,30 +48,29 @@ class Application
 		int px;
 		int py;
 		Ball() = default;
-		Ball(double initial_theta, double length, double radius, double m, int px, int py)
-			: theta(initial_theta), omega(0), alpha(0), len(length), x(0), y(0), r(radius), mass(m), px(px), py(py) {}
+		Ball(double initial_theta, double length, double m, int px, int py)
+			: theta(initial_theta), omega(0), alpha(0), len(length), x(0), y(0), r(0), mass(m), px(px), py(py) {}
 	};
 
-	class Window
-	{
+	class Window {
 	public:
 		Window(std::string title, int x, int y, int w, int h);
+		void init(std::string title, int x, int y, int w, int h);
 		~Window();
-		SDL_Window* getWindow() const { return this->window; };
-		SDL_Renderer* getRenderer() const { return this->renderer; };
-
 		int eventChecker();
 
 		void Renderer(std::vector<Ball>& b, int pressedButton) const;
-		void drawLeftMenu(std::vector<Ball>& b) const;
+		void drawLeftMenu(std::vector<Ball>& b, int pressedButton) const;
+		void drawInfo(std::vector<Ball>& b) const;
+		void drawControls(std::vector<Ball>& b) const;
 		void drawPlatform() const;
 		void drawBall(std::vector<Ball>& b) const;
 		void drawLineFromPlatformToBall(std::vector<Ball>& b) const;
 
 		void moveBall(std::vector<Ball>& b, double dt, int pressedButton);
 		void resolveCollision(std::vector<Ball>& b);
+		void changeRadiusFromMass(std::vector<Ball>& b);
 
-		void init(std::string title, int x, int y, int w, int h);
 		int w, h;
 		SDL_Window* window;
 		SDL_Renderer* renderer;
@@ -67,11 +83,25 @@ private:
 	void Run(std::string title, int x, int y, int w, int h) {
 		Window window(title, x, y, w, h);
 		std::vector<Ball> balls(2);
-		balls[0] = {0, 234, 20, 1, 2 * w / 3 - 35, h / 4};
-		balls[1] = {M_PI/2, 234, 30, 1000, 2 * w / 3 - 35, h / 4};
+		balls[0] = { 0, 234, 50, 2 * w / 3 - 35, h / 6 + h / 24 };
+		balls[1] = { M_PI / 2, 234, 50, 2 * w / 3 - 35, h / 6 + h / 24 };
 		int pressedButton = nothing;
 		double dt = 0.1;
 		while ((pressedButton = window.eventChecker()) != aexit) {
+			if (pressedButton < nothing)
+				if (pressedButton < 101) balls[0].mass = pressedButton;
+				else if (pressedButton < 201) balls[1].mass = pressedButton - 100;
+				else if (pressedButton < 562) balls[0].theta = DegToRad(pressedButton - 201);
+				else balls[1].theta = DegToRad(pressedButton - 562);
+			if (pressedButton == incMass1) balls[0].mass = (std::min(static_cast<int>(balls[0].mass) + 1, SLIDER1_MAX));
+			else if (pressedButton == decMass1) balls[0].mass = (std::max(static_cast<int>(balls[0].mass) - 1, SLIDER1_MIN));
+			else if (pressedButton == incMass2) balls[1].mass = (std::min(static_cast<int>(balls[1].mass) + 1, SLIDER1_MAX));
+			else if (pressedButton == decMass2) balls[1].mass = (std::max(static_cast<int>(balls[1].mass) - 1, SLIDER1_MIN));
+			else if (pressedButton == incAngle1) balls[0].theta = (DegToRad(RadToDeg(balls[0].theta) + 1));
+			else if (pressedButton == decAngle1) balls[0].theta = (DegToRad(RadToDeg(balls[0].theta) - 1));
+			else if (pressedButton == incAngle2) balls[1].theta = (DegToRad(RadToDeg(balls[1].theta) + 1));
+			else if (pressedButton == decAngle2) balls[1].theta = (DegToRad(RadToDeg(balls[1].theta) - 1));
+			SDL_Log("%d", pressedButton);
 			window.Renderer(balls, pressedButton);
 			window.moveBall(balls, dt, pressedButton);
 			SDL_Delay(static_cast<int>(dt));
